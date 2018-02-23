a9959ec85f9afa1f294db85b0210c3a2277b559e
Andrew Bregger <adb3649@tamu.edu>
2018-02-23 00:10:22 +0000

    Part 1: test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,9 +13,9 @@ class MoviesController < ApplicationController
   def index
     id = params[:id]
     if id == "title_header"
-      @movies = Movie.order(:title)
+      @movies = Movie.order("title ASC")
     else if id == "release_date_header"
-      @movies = Movie.order(:release_date)
+      @movies = Movie.order("release_date ASC")
     else
       @movies = Movie.all
     end
