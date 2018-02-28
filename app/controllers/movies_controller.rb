73f7a8bb30add2fc51a0db7bb2e81f797555b029
Andrew Bregger <adb3649@tamu.edu>
2018-02-28 20:17:42 +0000

    Task 1: Testing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,7 +16,7 @@ class MoviesController < ApplicationController
     sorts = params[:sort]
     
     if sorts != ""
-      @movies = Movie.order(sorts + " asc")
+      @movies = Movie.order(sorts)
     end
   end
 
