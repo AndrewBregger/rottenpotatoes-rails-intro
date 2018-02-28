9dd7facc4a2667358db783a9d52f05fe9e86151e
Andrew Bregger <adb3649@tamu.edu>
2018-02-28 23:20:01 +0000

    part 2 testing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,7 @@ class MoviesController < ApplicationController
 
   def index
     # this will be changed later
-    @all_ratings = Movie.order(:rating)
+    @all_ratings = Movie.order(:rating).pluck(:rating).uniq
     @checked_ratings = check
     @checked_ratings.each do |rating|
       params[rating] = true
